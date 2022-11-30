192d1b5de15f34dba996df1fa56794f66a7f52aa
Willian010 <jonatanwillianramos@gmail.com>
2022-11-30 10:21:14 -0300

    go

--- theend/src/App.js
+++ theend/src/App.js
@@ -1,11 +1,17 @@
 
 import './App.css';
 import {Login} from './Componentes/NavBar';
+import {Contador} from './Contador/Contador';
 
 function App() {
+  const agregar = (productos) => {
+    console.log("funcion agregar" , productos)
+  }
   return (
     <div className="App">
       <Login/>
+      <Contador stock={10} agregarProducto={agregar} />
+
 
     </div>
   );
